079650ae221a532a9e260b325ee203dc117a2de8
samburgess <saburgess239@gmail.com>
2020-04-04 15:31:35 -0700

    Updates

---
+++ precache-manifest.09d56acf10091530540d002efbd596b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcda06f12a1d0a38dd5c735490105ac",
+    "url": "/covidmap_client/index.html"
+  },
+  {
+    "revision": "6da40234a3917f2cbf41",
+    "url": "/covidmap_client/static/css/main.07395cd4.chunk.css"
+  },
+  {
+    "revision": "1044ed3206e86fe3ddd9",
+    "url": "/covidmap_client/static/js/2.16c2983e.chunk.js"
+  },
+  {
+    "revision": "2a65342a7cef87ae756f1549b040326b",
+    "url": "/covidmap_client/static/js/2.16c2983e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da40234a3917f2cbf41",
+    "url": "/covidmap_client/static/js/main.3178f4a4.chunk.js"
+  },
+  {
+    "revision": "f33bacabced4b2fb1859",
+    "url": "/covidmap_client/static/js/runtime-main.79342154.js"
+  }
+]);
